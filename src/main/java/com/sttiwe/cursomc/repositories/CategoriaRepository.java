538b878423e12a8d0f916df464cdbae7f3290c74
Sttiwe Sousa <sttiwew@gmail.com>
2020-08-28 08:40:35 -0300

    Criando repository e service para CAtegoria

---
+++ src/main/java/com/sttiwe/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,14 @@
+package com.sttiwe.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.sttiwe.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	
+	
+	
+}
