bc0b121fc441872d97e071dfe4b4db3440a773f2
Sttiwe Sousa <sttiwew@gmail.com>
2020-08-28 08:49:10 -0300

    Criando operacao de instanciacao

--- src/main/java/com/sttiwe/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/sttiwe/cursomc/resources/CategoriaResource.java
@@ -1,10 +1,5 @@
 package com.sttiwe.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.websocket.server.PathParam;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
