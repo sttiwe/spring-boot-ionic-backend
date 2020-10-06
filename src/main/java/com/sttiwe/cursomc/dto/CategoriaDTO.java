f8f4739dfe1ce315d8f798c9a958c3374c4d4897
Sttiwe Sousa <sttiwew@gmail.com>
2020-10-05 21:07:32 -0300

    Listandos todas categorias com DTO

---
+++ src/main/java/com/sttiwe/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,45 @@
+package com.sttiwe.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sttiwe.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	
+	public CategoriaDTO() {}
+	
+	
+	public CategoriaDTO(Categoria obj) {
+		id = obj.getId();
+		nome = obj.getNome();				
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+
+}
--- src/main/java/com/sttiwe/cursomc/services/CategoriaService.java
+++ src/main/java/com/sttiwe/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.sttiwe.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,4 +42,9 @@ public class CategoriaService {
 		}
 		
 	}
+	
+	public List<Categoria> findAll(){
+		return repo.findAll();
+	}
+	
 }
