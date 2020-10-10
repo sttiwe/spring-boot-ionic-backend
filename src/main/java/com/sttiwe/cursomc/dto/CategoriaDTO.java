06cb49b1d26755892d97a23cea313a12bb761624
Sttiwe Sousa <sttiwew@gmail.com>
2020-10-10 10:03:45 -0300

    Validacao sintatica com Bean Validation

--- src/main/java/com/sttiwe/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/sttiwe/cursomc/dto/CategoriaDTO.java
@@ -2,18 +2,19 @@ package com.sttiwe.cursomc.dto;
 
 import java.io.Serializable;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
 
 import com.sttiwe.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "O tamanho deve ser entre 5 e 80 carecteres")
 	private String nome;
 	
 	public CategoriaDTO() {}
--- src/main/java/com/sttiwe/cursomc/services/CategoriaService.java
+++ src/main/java/com/sttiwe/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.sttiwe.cursomc.domain.Categoria;
+import com.sttiwe.cursomc.dto.CategoriaDTO;
 import com.sttiwe.cursomc.repositories.CategoriaRepository;
 import com.sttiwe.cursomc.services.exceptions.DataIntegrityException;
 import com.sttiwe.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,6 +54,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
