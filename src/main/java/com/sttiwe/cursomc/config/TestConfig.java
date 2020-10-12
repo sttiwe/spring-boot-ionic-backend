2eabdb2e338451b79e2e1f4a705d205bafa9a97a
Sttiwe Sousa <sttiwew@gmail.com>
2020-10-12 12:09:10 -0300

    Criando profile de teste

---
+++ src/main/java/com/sttiwe/cursomc/config/TestConfig.java
@@ -0,0 +1,22 @@
+package com.sttiwe.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.sttiwe.cursomc.services.DBService;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Bean
+	public boolean instantiateDataBase() throws Exception {
+		dbService.instantiateTestDatabase();				
+		return true;
+	}
+}
